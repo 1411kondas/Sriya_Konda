2dce9871871aa3974069ea18e85be71090036242
kittyTyree <tyreek@carleton.edu>
2021-10-21 01:02:31 -0500

    Successful completion of the first part of the olympics dataset assignments.

--- olympics/convert.py
+++ olympics/convert.py
@@ -1,131 +1,233 @@
-data_set = open("athlete_events.csv")
-data_set.readline()
-# athletes_writer = open('athletes.csv', 'w')
-# fieldnames = ['athlete_id','name', 'age', 'height', 'weight', 'sex']
-# athletes_writer.write(",".join(fieldnames) + '\n')
-# id_num = 0
-# for line in data_set:
-#     fields = line.split(",")
-#     athlete_id, name, sex, age, height, weight, team, NOC, games, year, season, city, sport, event, medal = str(fields[0]), fields[1], fields[2], str(fields[3]), str(fields[4]), str(fields[5]), fields[6], fields [7], fields[8], fields[9], fields[10], fields[11], fields[12], fields[13], fields[14]
-#     athletes_list = [athlete_id, name, age, height, weight, sex]
-#
-#     if fields[0] != id_num:
-#         athletes_writer.write(",".join(athletes_list) + '\n')
-#         id_num = fields[0]
-#     else:
-#         continue
+'''
+    October 19th 2021.
+    Kitty Tyree
+    Olympics database design for cs257 in fall 2021
+'''
 
-# team_writer = open('team.csv', 'w')
-# fieldnames = ['team_id','team_name']
-# team_writer.write(",".join(fieldnames) + '\n')
-# team_name_str_lst = []
-# counter = 1
-# for line in data_set:
-#     fields = line.split(",")
-#     athlete_id, name, sex, age, height, weight, team, NOC, games, year, season, city, sport, event, medal = str(fields[0]), fields[1], fields[2], str(fields[3]), str(fields[4]), str(fields[5]), fields[6], fields [7], fields[8], fields[9], fields[10], fields[11], fields[12], fields[13], fields[14]
-#     team_list = [str(counter), team]
-#
-#     if fields[6] not in team_name_str_lst:
-#         team_writer.write(",".join(team_list) + '\n')
-#         team_name_str_lst.append(fields[6])
-#         id_num = fields[0]
-#         counter = counter + 1
-#     else:
-#         continue
+import csv
 
-# NOC_writer = open('NOC.csv', 'w')
-# fieldnames = ['NOC_id','NOC_name']
-# NOC_writer.write(",".join(fieldnames) + '\n')
-# NOC_name_str_lst = []
-# counter = 1
-# for line in data_set:
-#     fields = line.split(",")
-#     athlete_id, name, sex, age, height, weight, team, NOC, games, year, season, city, sport, event, medal = str(fields[0]), fields[1], fields[2], str(fields[3]), str(fields[4]), str(fields[5]), fields[6], fields [7], fields[8], fields[9], fields[10], fields[11], fields[12], fields[13], fields[14]
-#     team_list = [str(counter), NOC]
-#
-#     if fields[7] not in NOC_name_str_lst:
-#         NOC_writer.write(",".join(team_list) + '\n')
-#         NOC_name_str_lst.append(fields[7])
-#         counter = counter + 1
-#     else:
-#         continue
-#
-# writer = open('sport.csv', 'w')
-# fieldnames = ['sport_id','sport_name']
-# writer.write(",".join(fieldnames) + '\n')
-# name_str_lst = []
-# counter = 1
-# for line in data_set:
-#     fields = line.split(",")
-#     athlete_id, name, sex, age, height, weight, team, NOC, games, year, season, city, sport, event, medal = str(fields[0]), fields[1], fields[2], str(fields[3]), str(fields[4]), str(fields[5]), fields[6], fields [7], fields[8], fields[9], fields[10], fields[11], fields[12], fields[13], fields[14]
-#     item_list = [str(counter), sport]
-#
-#     if fields[12] not in name_str_lst:
-#         writer.write(",".join(item_list) + '\n')
-#         name_str_lst.append(fields[12])
-#         counter = counter + 1
-#     else:
-#         continue
-
-# writer = open('event.csv', 'w')
-# fieldnames = ['event_id','event_name']
-# writer.write(",".join(fieldnames) + '\n')
-# name_str_lst = []
-# counter = 1
-# for line in data_set:
-#     fields = line.split(",")
-#     athlete_id, name, sex, age, height, weight, team, NOC, games, year, season, city, sport, event, medal = str(fields[0]), fields[1], fields[2], str(fields[3]), str(fields[4]), str(fields[5]), fields[6], fields [7], fields[8], fields[9], fields[10], fields[11], fields[12], fields[13], fields[14]
-#     item_list = [str(counter), event]
-#
-#     if fields[13] not in name_str_lst:
-#         writer.write(",".join(item_list) + '\n')
-#         name_str_lst.append(fields[13])
-#         counter = counter + 1
-#     else:
-#         continue
-
-# writer = open('medal.csv', 'w')
-# fieldnames = ['medal_id','medal']
-# writer.write(",".join(fieldnames) + '\n')
-# name_str_lst = []
-# writer.write('1,NA\n')
-# writer.write('2,"Bronze"\n')
-# writer.write('3,"Silver"\n')
-# writer.write('4,"Gold"\n')
-
-# writer = open('games.csv', 'w')
-# fieldnames = ['games_id','games_name','season','year']
-# writer.write(",".join(fieldnames) + '\n')
-# name_str_lst = []
-# counter = 1
-# for line in data_set:
-#     fields = line.split(",")
-#     athlete_id, name, sex, age, height, weight, team, NOC, games, year, season, city, sport, event, medal = str(fields[0]), fields[1], fields[2], str(fields[3]), str(fields[4]), str(fields[5]), fields[6], fields [7], fields[8], fields[9], fields[10], fields[11], fields[12], fields[13], fields[14]
-#     item_list = [str(counter), games,season,year]
-#
-#     if fields[8] not in name_str_lst:
-#         writer.write(",".join(item_list) + '\n')
-#         name_str_lst.append(fields[8])
-#         counter = counter + 1
-#     else:
-#        continue
-
-writer = open('cities.csv', 'w')
-fieldnames = ['city_id','city_name']
-writer.write(",".join(fieldnames) + '\n')
-name_str_lst = []
-counter = 1
-for line in data_set:
-    fields = line.split(",")
-    athlete_id, name, sex, age, height, weight, team, NOC, games, year, season, city, sport, event, medal = str(fields[0]), fields[1], fields[2], str(fields[3]), str(fields[4]), str(fields[5]), fields[6], fields [7], fields[8], fields[9], fields[10], fields[11], fields[12], fields[13], fields[14]
-    item_list = [str(counter), city]
-
-    if fields[11] not in name_str_lst:
-        writer.write(",".join(item_list) + '\n')
-        name_str_lst.append(fields[11])
-        counter = counter + 1
-    else:
-        continue
+# MAP AN ATHLETE ID TO THE ATHLETE'S NAME
+# why are we only mapping athlete id to their name? because if they competed
+# in multiple games, their weight, and age and other body stats may have changed
+# I am not tying them to their sex either, I cannot be sure that
+# doesn't change across olympics.
+athletes = {}
+csv_file = open('athlete_events.csv')
+reader = csv.reader(csv_file)
+athletes_file = open('athletes.csv', 'w')
+writer = csv.writer(athletes_file)
+next(reader)
+for row in reader:
+    athlete_id = row[0] #this is a string
+    athlete_name = row[1]
+    if athlete_id not in athletes: #Have I seen the athlete before? No? great! do the thing!
+        athletes[athlete_id] = athlete_name  #mapping ids to athlete names
+        writer.writerow([athlete_id, athlete_name]) #saving in athletes.csv
+csv_file.close()
+athletes_file.close()
 
 
-data_set.close()
+# MAP AN EVENT TO A CREATED EVENT ID
+# why are we doing that? because we don't have event id's listed anywhere in
+# connection to the athletes, we just have the names of the events.
+# So to make it easier, make them accessable by name so we can later match
+# to the event and get the ID
+
+events = {}
+csv_file = open('athlete_events.csv')
+reader = csv.reader(csv_file)
+events_file = open('events.csv', 'w')
+writer = csv.writer(events_file)
+next(reader)
+id_counter = 1
+for row in reader:
+    event_name = row[13]
+    if event_name not in events: #Have I seen the event before? No? great! do the thing!
+        event_id = id_counter
+        id_counter += 1
+        events[event_name] = event_id
+        writer.writerow([event_id, event_name])
+events_file.close()
+csv_file.close()
+
+
+# MAP A SPORT TO A CREATED SPORT ID
+sports = {}
+csv_file = open('athlete_events.csv')
+reader = csv.reader(csv_file)
+sports_file = open('sports.csv', 'w')
+writer = csv.writer(sports_file)
+next(reader)
+id_counter = 1
+for row in reader:
+    sport_name = row[12]
+    if sport_name not in sports: #Have I seen the event before? No? great! do the thing!
+        sport_id = id_counter
+        id_counter += 1
+        sports[sport_name] = sport_id
+        writer.writerow([sport_id, sport_name])
+sports_file.close()
+csv_file.close()
+
+
+# MATCHING EVENTS TO THEIR SPORT
+events_sport = {}
+csv_file = open('athlete_events.csv')
+reader = csv.reader(csv_file)
+events_sport_file = open('events_sport.csv', 'w')
+writer = csv.writer(events_sport_file)
+next(reader)
+id_counter = 1
+for row in reader:
+    sport_name = row[12]
+    sport_id = sports[sport_name]
+    event_name = row[13]
+    event_id = events[event_name]
+    if event_id not in events_sport: #Have I seen the event before? No? great! do the thing!
+        events_sport[event_id] = sport_id
+        writer.writerow([event_id, sport_id])
+events_sport_file.close()
+csv_file.close()
+
+
+
+# I want an olympic games file with identifying Ids
+# MAP A GAME TO A CREATED GAME ID
+
+
+games = {}
+csv_file = open('athlete_events.csv')
+reader = csv.reader(csv_file)
+games_file = open('games.csv', 'w')
+writer = csv.writer(games_file)
+next(reader)
+id_counter = 1
+for row in reader:
+    games_name = row[8]
+    year = row[9]
+    season = row[10]
+    city = row[11]
+    if games_name not in games: #Have I seen the games before? No? great! do the thing!
+        games_id = id_counter
+        id_counter += 1
+
+        games[games_name] = {"games_id":games_id, "year": year, "season": season, "city": city}
+        writer.writerow([games_id, games_name, year, season, city])
+games_file.close()
+csv_file.close()
+
+
+
+
+# MAP A TEAM NAME TO AN ID
+teams = {}
+csv_file = open('athlete_events.csv')
+reader = csv.reader(csv_file)
+teams_file = open('teams.csv', 'w')
+writer = csv.writer(teams_file)
+next(reader)
+id_counter = 1
+for row in reader:
+    team_name = row[6]
+    if team_name not in teams: #Have I seen the team before? No? great! do the thing!
+        team_id = id_counter
+        id_counter += 1
+
+        teams[team_name] = team_id
+        writer.writerow([team_id, team_name])
+teams_file.close()
+csv_file.close()
+
+
+# MAP A NOC TO AN ID
+
+NOC_regions = {}
+csv_file = open('athlete_events.csv')
+reader = csv.reader(csv_file)
+NOC_regions_file = open('NOC_regions.csv', 'w')
+writer = csv.writer(NOC_regions_file)
+next(reader)
+id_counter = 1
+for row in reader:
+    NOC_name = row[7]
+    if NOC_name not in NOC_regions: #Have I seen the team before? No? great! do the thing!
+        NOC_id = id_counter
+        id_counter += 1
+
+        NOC_regions[NOC_name] = NOC_id
+        writer.writerow([NOC_id, NOC_name])
+NOC_regions_file.close()
+csv_file.close()
+
+
+# MAP TEAM IDS TO NOC IDS
+team_NOC = {}
+csv_file = open('athlete_events.csv')
+reader = csv.reader(csv_file)
+team_NOC_file = open('teamNames_NOC.csv', 'w')
+writer = csv.writer(team_NOC_file)
+next(reader)
+for row in reader:
+    team_name = row[6]
+    team_id = teams[team_name]
+    NOC_name = row[7]
+    NOC_id = NOC_regions[NOC_name]
+    if team_id not in team_NOC:
+        team_NOC[team_id] = NOC_id
+        writer.writerow([team_id, NOC_id])
+team_NOC_file.close()
+csv_file.close()
+
+
+# TIE ATHLETE IDS TO EVENT IDS AND THEIR MEDALS
+# For each row in the original athlete_events.csv file, build a row for
+# the event_results.csv table
+csv_file = open('athlete_events.csv')
+reader = csv.reader(csv_file)
+event_results_file = open('event_results.csv', 'w')
+writer = csv.writer(event_results_file)
+next(reader)
+for row in reader:
+    athlete_id = row[0]
+    event_name = row[13]
+    event_id = events[event_name] # this goes into the events dictionary and retrieves the ID number
+    games_name = row[8]
+    games_id = games[games_name]["games_id"]
+    medal = row[14]
+    writer.writerow([athlete_id, games_id, event_id, medal])
+event_results_file.close()
+csv_file.close()
+
+
+
+# I want a file that has the athlete stats at each olympic games
+# Includes athlete_id, games_id, team_id, athlete_sex, athlete-age, athlete_height, athlete_weight
+
+csv_file = open('athlete_events.csv')
+reader = csv.reader(csv_file)
+athlete_stats_file = open('athlete_stats.csv', 'w')
+writer = csv.writer(athlete_stats_file)
+next(reader)
+
+# I need it to only go if the athlete for that specific game has yet to be counted
+doubles_list=[]
+curr_athlete_id = 1
+for row in reader:
+    athlete_id = row[0]
+    if athlete_id != curr_athlete_id:
+        doubles_list.clear()
+        curr_athlete_id = athlete_id
+    athlete_sex = row[2]
+    athlete_age = row[3]
+    athlete_height = row[4]
+    athlete_weight = row[5]
+    team_name = row[6]
+    team_id = teams[team_name]
+    games_name = row[8]
+    games_id = games[games_name]["games_id"]
+    athlete_id_game_double = (athlete_id,games_id)
+    if athlete_id_game_double not in doubles_list:
+        writer.writerow([athlete_id, games_id, team_id, athlete_sex, athlete_age, athlete_height, athlete_weight])
+        doubles_list.append(athlete_id_game_double)
+athlete_stats_file.close()
+csv_file.close()
--- olympics/olympics-schema.sql
+++ olympics/olympics-schema.sql
@@ -1,43 +1,82 @@
 CREATE TABLE athletes (
 athlete_id INTEGER,
-name TEXT,
-age INTEGER,
-height INTEGER,
-weight INTEGER,
-sex TEXT);
+athlete_name TEXT);
 
-CREATE TABLE cities (
-city_id INTEGER,
-city_name TEXT);
-
-CREATE TABLE event (
+CREATE TABLE events (
 event_id INTEGER,
 event_name TEXT);
+
+CREATE TABLE event_results (
+athlete_id INTEGER,
+games_id INTEGER,
+event_id INTEGER,
+medal TEXT);
 
 CREATE TABLE games (
 games_id INTEGER,
 games_name TEXT,
+year INTEGER,
 season TEXT,
-year INTEGER);
+city TEXT);
 
-CREATE TABLE medal (
-medal_id INTEGER,
-medal TEXT);
-
-CREATE TABLE NOC (
-NOC_id Integer,
-NOC_name TEXT);
-
-CREATE TABLE sport (
-sport_id INTEGER,
-sport_name TEXT);
-
-CREATE TABLE team (
+CREATE TABLE teams (
 team_id INTEGER,
 team_name TEXT);
 
-SELECT NOC_name
-from NOC
-ORDER BY NOC_name;
+CREATE TABLE NOC_regions (
+NOC_id INTEGER,
+NOC_name TEXT);
 
+CREATE TABLE teamNames_NOC (
+team_id INTEGER,
+NOC_id INTEGER);
 
+CREATE TABLE athlete_stats (
+athlete_id INTEGER,
+games_id INTEGER,
+team_id INTEGER,
+athlete_sex TEXT,
+athlete_age INTEGER,
+athlete_height FLOAT,
+athlete_weight FLOAT);
+
+CREATE TABLE sports (
+sport_id INTEGER,
+sport_name TEXT);
+
+CREATE TABLE events_sport (
+event_id INTEGER,
+sport_id INTEGER);
+
+SELECT noc_regions.NOC_name
+FROM noc_regions
+ORDER BY noc_regions.NOC_name;
+
+SELECT DISTINCT athletes.athlete_name
+FROM athletes, athlete_stats, teams
+WHERE athletes.athlete_id = athlete_stats.athlete_id
+AND teams.team_id = athlete_stats.team_id
+AND teams.team_name = 'Kenya'
+ORDER BY athletes.athlete_name;
+
+SELECT events.event_name, games.city, games.season, games.year, event_results.medal
+FROM events, event_results, games, athletes
+Where athletes.athlete_name = 'Gregory Efthimios "Greg" Louganis'
+AND athletes.athlete_id = event_results.athlete_id
+AND event_results.event_id = events.event_id
+AND event_results.games_id = games.games_id
+AND event_results.medal IS NOT NULL
+ORDER BY games.year, events.event_name;
+
+SELECT noc_regions.noc_name, COUNT(event_results.medal)
+FROM noc_regions, event_results, athletes, athlete_stats, teams, teamnames_noc
+WHERE noc_regions.noc_id = teamnames_noc.noc_id
+AND event_results.athlete_id = athletes.athlete_id
+AND event_results.athlete_id = athlete_stats.athlete_id
+AND event_results.games_id = athlete_stats.games_id
+AND athletes.athlete_id = athlete_stats.athlete_id
+AND athlete_stats.team_id = teams.team_id
+AND teams.team_id = teamnames_noc.team_id
+AND event_results.medal = 'Gold'
+GROUP BY noc_regions.noc_name
+ORDER BY COUNT(event_results.medal) DESC, noc_regions.noc_name;
