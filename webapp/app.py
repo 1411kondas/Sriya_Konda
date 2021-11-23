72a60d5724706a48dddbecd1a1e7acde9dccce32
kittyTyree <tyreek@carleton.edu>
2021-11-23 13:19:04 -0600

    finishing touches on webapp assignment

--- webapp/app.py
+++ webapp/app.py
@@ -1,10 +1,8 @@
 '''
     app.py
     Sriya Konda & Kitty Tyree
-    fall 2021
-
-    A small Flask application that provides a barelywebsite with an accompanying
-    API (which is also tiny) to support that website.
+    For use in the "webapp" assignment for Carleton's
+    CS 257 Software Design class, Fall 2021.
 '''
 import flask
 import argparse
@@ -14,28 +12,32 @@ app = flask.Flask(__name__, static_folder='static', template_folder='templates')
 app.register_blueprint(api.api, url_prefix='/api')
 
 @app.route('/')
-
 @app.route('/home')
 def home():
-    return flask.render_template('mockup1.html')
+    return flask.render_template('homepage.html')
+
 
 @app.route('/artists')
 def artists():
-    return flask.render_template('mockup2.html')
+    return flask.render_template('artist_search.html')
+
 
 @app.route('/top_200')
 def chart():
-    return flask.render_template('mockup3.html')
+    return flask.render_template('date_search.html')
+
 
 @app.route('/generate_playlist')
 def make_playlist():
-    return flask.render_template('mockup4.html')
+    return flask.render_template('generate_playlist.html')
+
 
 
 @app.route('/help')
 def help():
     help_text = open('templates/help.txt').read()
     return flask.Response(help_text, mimetype='text/plain')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('A spotify application, including API & DB')
